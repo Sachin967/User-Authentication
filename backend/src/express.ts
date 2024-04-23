76ee7fa56d83c995e74809f1c773874205b5e77b
Sachin967 <sachinsachu967@gmail.com>
2024-04-23 20:19:16 +0530

    cors modified

--- backend/src/express.ts
+++ backend/src/express.ts
@@ -7,12 +7,19 @@ const expressApp = async (app: Application): Promise<void> => {
      app.use(express.json({ limit: '10mb' }))
      app.use(express.urlencoded({ extended: true, limit: '10mb' }))
      app.use(cookieParser())
-     const corsOptions = {
-          origin:  'https://userauth.sachinms.fyi',
-          credentials: true,
-     }
+     // const corsOptions = {
+     //      origin:  'https://userauth.sachinms.fyi',
+     //      credentials: true,
+     // }
 
-     app.use(cors(corsOptions))
+     // app.use(cors(corsOptions))
+     app.use(
+          cors({
+               origin: 'hhttps://userauth.sachinms.fyi',
+               methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+               credentials: true,
+          })
+     )
 }
 
 export default expressApp
